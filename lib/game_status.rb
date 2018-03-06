7cfc82e715961fc66a91945e386f40395bef2e49
Jerry P <jpull@jpull.com>
2018-03-06 03:13:40 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -49,6 +49,7 @@ def won?(board = ["X", " ", " ", " ", " ", " ", " ", " ", " "])
   position_2 = board[win_index_2] # load the value of the board at win_index_2
   position_3 = board[win_index_3] # load the value of the board at win_index_3
 puts "START2"
+puts "win_comb #{win_combination}"
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
     puts win_combination # return the win_combination indexes that won.
   else
