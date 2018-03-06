3ea6c12a3bc711cfb62d7d4a1eb2013afd794119
Jerry P <jpull@jpull.com>
2018-03-06 03:46:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,9 +35,7 @@ def won?(board)
       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
         return win_combination # return the win_combination indexes that won.
       else
-        false
       end
-      false
     end
     false
   end
