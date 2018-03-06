6fd3878865c1f3f7ace0c8780b85ab00911db098
Jerry P <jpull@jpull.com>
2018-03-06 03:20:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,27 +18,23 @@ WIN_COMBINATIONS = [
 def won?(board)
   if board.any?{|i| i == "X"} || board.any?{|i| i == "0"}
     WIN_COMBINATIONS.each do |win_combination|
-  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-  # grab each index from the win_combination that composes a win.
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-
-  position_1 = board[win_index_1] # load the value of the board at win_index_1
-  position_2 = board[win_index_2] # load the value of the board at win_index_2
-  position_3 = board[win_index_3] # load the value of the board at win_index_3
-puts "START2"
-puts "win_combination #{win_combination}"
-puts "position_1 #{position_1} position_2 #{position_2} position_3 #{position_3}"
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    puts "win: #{win_combination}"
-    return win_combination # return the win_combination indexes that won.
-  else
-    puts "FALSE"
-    false
+      # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+      # grab each index from the win_combination that composes a win.
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      
+      position_1 = board[win_index_1] # load the value of the board at win_index_1
+      position_2 = board[win_index_2] # load the value of the board at win_index_2
+      position_3 = board[win_index_3] # load the value of the board at win_index_3
+      
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combination # return the win_combination indexes that won.
+      else
+        false
+      end
+    end
   end
-end
-end
 else
   false
 end
