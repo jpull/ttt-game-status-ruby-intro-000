44654cfcb814f31234707a8ec0173c517702fc80
Jerry P <jpull@jpull.com>
2018-03-06 03:16:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  if board.any?{|i| i != " "}
+  #if board.any?{|i| i != " "}
     WIN_COMBINATIONS.each do |win_combination|
   # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
   # grab each index from the win_combination that composes a win.
@@ -36,7 +36,6 @@ puts "position_1 #{position_1} position_2 #{position_2} position_3 #{position_3}
     puts "FALSE"
     false
   end
-end
 end
 end
 
